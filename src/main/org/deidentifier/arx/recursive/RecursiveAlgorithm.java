e65b32659065b6efa14c0c96f433948afb0ec450
fstahnke <github@fabian-stahnke.de>
2015-07-01 16:29:25 +0200

    First running version

--- src/main/org/deidentifier/arx/recursive/RecursiveAlgorithm.java
+++ src/main/org/deidentifier/arx/recursive/RecursiveAlgorithm.java
@@ -1,5 +1,92 @@
 package org.deidentifier.arx.recursive;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.ARXResult;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.Data.DefaultData;
+import org.deidentifier.arx.DataHandle;
+import org.deidentifier.arx.utility.DataConverter;
+
 public class RecursiveAlgorithm {
+
+    public void execute(final Data data, final ARXConfiguration config, final ARXAnonymizer anonymizer) throws IOException
+    {
+        // Execute the first anonymization
+        ARXResult result = anonymizer.anonymize(data, config);
+        // Get handle for input data and result
+        DataHandle inHandle = data.getHandle();
+        DataHandle outHandle = result.getOutput(false);
+        
+        // Convert input and output to array of string arrays
+        String[][] input = new DataConverter().toArray(inHandle);
+        String[][] output = new DataConverter().toArray(outHandle);
+        
+        // Create new data object for next anonymization step
+        DefaultData outliers = Data.create();
+        outliers.add(inHandle.iterator().next()); // add header to outlier object
+        setupDataObject(outliers);
+        // Declare list of rows that are currently suppressed
+        // Be careful to always consider the header and skip it if necessary
+        List<Integer> rows = new ArrayList<Integer>();
+        // Create iterator for string arrays to iterate rows
+        Iterator<String[]> rowIter = inHandle.iterator();
+        rowIter.next(); // Skip header
+        
+        /* Iterate all rows of the input
+         * if a row is suppressed (an outlier), add it to the new data object
+         * and add the number of the row to the list
+         */
+        for (int i = 0; i < outHandle.getNumRows(); i++) {
+            String[] currentRow = rowIter.next();
+            if (outHandle.isOutlier(i)) {
+                outliers.add(currentRow);
+                System.out.println(Arrays.toString(currentRow));
+                rows.add(i);
+            }
+        }
+        System.out.println(rows.size());
+        if (rows.size() >= 2)
+        // Anonymize the outliers and get the handle for the result
+        result = anonymizer.anonymize(outliers, config);
+        outHandle = result.getOutput(false);
+        
+        rowIter = outHandle.iterator();
+        for (ListIterator<Integer> intIter = rows.listIterator(); intIter.hasNext();) {
+            output[intIter.next()] = rowIter.next();
+        }
+        
+        for (int i = 0; i < output.length; i++) {
+            System.out.println(Arrays.toString(output[i]));
+        }
+        
+        
+    }
+    
+    private void setupDataObject(Data data) throws IOException {
+
+        
+        // Define input files
+        data.getDefinition()
+            .setAttributeType("age",
+                              Hierarchy.create("data/test_hierarchy_age.csv",
+                                               ';'));
+        data.getDefinition()
+            .setAttributeType("gender",
+                              Hierarchy.create("data/test_hierarchy_gender.csv",
+                                               ';'));
+        data.getDefinition()
+            .setAttributeType("zipcode",
+                              Hierarchy.create("data/test_hierarchy_zipcode.csv",
+                                               ';'));
+    }
     
 }
--- src/main/org/deidentifier/arx/recursive/RecursiveTest.java
+++ src/main/org/deidentifier/arx/recursive/RecursiveTest.java
@@ -1,12 +1,45 @@
 package org.deidentifier.arx.recursive;
 
+import java.io.IOException;
+
+import org.deidentifier.arx.ARXAnonymizer;
+import org.deidentifier.arx.ARXConfiguration;
+import org.deidentifier.arx.Data;
+import org.deidentifier.arx.AttributeType.Hierarchy;
+import org.deidentifier.arx.criteria.KAnonymity;
+
 public class RecursiveTest {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         RecursiveAlgorithm recursiveInstance = new RecursiveAlgorithm();
+        Data data = Data.create("data/test.csv", ';');
+        
+        // Define input files
+        data.getDefinition()
+            .setAttributeType("age",
+                              Hierarchy.create("data/test_hierarchy_age.csv",
+                                               ';'));
+        data.getDefinition()
+            .setAttributeType("gender",
+                              Hierarchy.create("data/test_hierarchy_gender.csv",
+                                               ';'));
+        data.getDefinition()
+            .setAttributeType("zipcode",
+                              Hierarchy.create("data/test_hierarchy_zipcode.csv",
+                                               ';'));
         
         
+        ARXAnonymizer anonymizer = new ARXAnonymizer();
+        
+        ARXConfiguration config = ARXConfiguration.create();
+        
+
+        config.addCriterion(new KAnonymity(2));
+        config.setMaxOutliers(0.3d);
+        
+        
+        recursiveInstance.execute(data, config, anonymizer);
         
     }
     
